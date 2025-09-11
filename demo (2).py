21d21ae2dce4efe4c98d7d6fcd4689e609e57520
preetidanke <preetidanke@gmail.com>
2025-09-11 18:29:28 +0530

    demo2

---
+++ demo (2).py
@@ -0,0 +1,43 @@
+# comments - ignored by interpreter 
+# a = 10
+# b = 20
+# print(a+b)
+
+# print(hello world) # SyntaxError: invalid syntax
+print(9) # number - int
+print(9.5) # number - float
+a = 10
+print(a)
+greetings = "good morning" # text - string
+print(greetings)
+print('hello world')
+
+
+# import demo
+import keyword
+print(keyword.kwlist)
+
+# Identifiers
+data = "hello"
+# 2daydata = "hello" -> invalid, as it starts with digit
+_2daydata = "hello"
+
+a = 20
+A = 30
+
+print(a)
+print(A)
+
+# class = "some data" -> keywords cannot be identifiers
+# print(class)
+
+# Define Some Student Data
+# Bad way of using identifiers
+x = "Ravi"
+y = 25
+z = "Python Course"
+
+# Good way of using identifiers
+student_firstname = "Ravi"
+student_age = 25
+student_course = "Python Course"
